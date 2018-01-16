895a61f75b6021f0db9b44c184773277d85331f8
YEC\Garrett.May <maygarrett@hotmail.com>
2018-01-16 10:50:17 -0500

    UPDATE - Included gun model and some particle systems, working raycast and particles for gun firing

--- VR Game Project/Assets/Scripts/GunShot.cs
+++ VR Game Project/Assets/Scripts/GunShot.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class GunShot : MonoBehaviour {
 
     [SerializeField] private Transform _raycastOrigin;
+    [SerializeField] private GameObject _handgun;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField] private GameObject _gunSparks1;
+    [SerializeField] private GameObject _gunSparks2;
+    private int _gunSparksCounter = 1;
+    private ParticleSystem _gunSparks1System;
+    private ParticleSystem _gunSparks2System;
+
+
+
+    private Animation _gunAnimation;
+
+    // Use this for initialization
+    void Start () {
+
+        _gunAnimation = _handgun.GetComponent<Animation>();
+
+        _gunSparks1System = _gunSparks1.GetComponent<ParticleSystem>();
+        _gunSparks2System = _gunSparks2.GetComponent<ParticleSystem>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,11 +36,28 @@ public class GunShot : MonoBehaviour {
             RaycastHit hit;
 
             Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward, out hit, 1000.0f);
-            Debug.Log("Bang Bang");
 
+            _gunAnimation.Play();
+
+            if(hit.transform.gameObject.tag == "Metal")
+            {
+                if(_gunSparksCounter == 1)
+                {
+                    _gunSparks1.transform.position = hit.point;
+                    _gunSparks1System.Play();
+                    _gunSparksCounter = 2;
+                }
+                if(_gunSparksCounter == 2)
+                {
+                    _gunSparks2.transform.position = hit.point;
+                    _gunSparks2System.Play();
+                    _gunSparksCounter = 1;
+                }
+            }
+                
         }
 
-        Debug.DrawRay(_raycastOrigin.position, _raycastOrigin.forward, Color.red, 100.0f);
+        Debug.DrawLine(_raycastOrigin.position, _raycastOrigin.forward * 100, Color.red);
 
     }
 }
